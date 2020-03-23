da6f0e191875cb3d9d28c64f0b24ec797b4a02d7
Kozel_Pavel <pavelshherba@gmail.com>
2020-03-23 15:09:41 +0300

    feat: add subtask2

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,25 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var allElementSum = 0
+        var minOut: Int
+        var maxOut = 0
+
+        for (item in input) {
+            allElementSum += item
+        }
+
+        minOut = allElementSum
+        for (item in input) {
+            val currentSum = allElementSum - item
+
+            if (currentSum > maxOut) {
+                maxOut = currentSum
+            } else if (currentSum < minOut) {
+                minOut = currentSum
+            }
+        }
+
+        return intArrayOf(minOut, maxOut)
     }
 }
