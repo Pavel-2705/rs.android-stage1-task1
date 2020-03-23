0b691da3d37f3edb4df1cd80e14b87f0ff24fb14
Kozel_Pavel <pavelshherba@gmail.com>
2020-03-23 17:51:14 +0300

    init: start subtask4

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -4,6 +4,29 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val delimiter = charArrayOf(
+            '<', '>',
+            '[', ']',
+            '(', ')'
+        )
+        val myIndexes = intArrayOf()
+
+        inputString.forEachIndexed { index, char ->
+            when (char) {
+                delimiter[0] -> myIndexes[0] = (index)
+                delimiter[1] -> myIndexes[1] = (index)
+                delimiter[2] -> myIndexes[2] = (index)
+                delimiter[3] -> myIndexes[3] = (index)
+                delimiter[4] -> myIndexes[4] = (index)
+                delimiter[5] -> myIndexes[5] = (index)
+            }
+        }
+
+        val output = mutableListOf<String>()
+        output.add(inputString.substring(myIndexes[0], myIndexes[1]))
+        output.add(inputString.substring(myIndexes[2], myIndexes[3]))
+        output.add(inputString.substring(myIndexes[3], myIndexes[4]))
+
+        return output.toTypedArray()
     }
 }
