aa26a76953a1c9bc599ead88a16197b0631ba66c
Kozel_Pavel <pavelshherba@gmail.com>
2020-03-24 20:51:43 +0300

    init: start subtask5

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -4,6 +4,15 @@ class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        var wrong = 0
+
+        for (i in 0..(n - 1) / 2) {
+            if (digitString[i] != digitString[n - 1 - i]) {
+                wrong++
+            }
+        }
+
+        if (wrong > k) return "-1"
+        return "test"
     }
 }
